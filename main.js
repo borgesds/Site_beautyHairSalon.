e20d0e4d3f3aaa90dfdecff95eb0c5c9d3813be0
borgesds <borges.an@hotmail.com>
2022-05-08 10:09:12 -0300

    up Contact, Scrollreveal and main.js

--- main.js
+++ main.js
@@ -40,3 +40,19 @@ const swiper = new Swiper('.swiper', {
     mousewheel: true,
     Keyboard: true
 });
+
+/* ScrollReveal: Mostrar elementos quando der scroll na pagina */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text, 
+    #about .image, #about .text, 
+    #services header, #services .card, 
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links
+    `, {interval: 100})
