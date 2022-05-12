baaba0e6c2c13c300c8dea1fd312fb475ee87227
borgesds <borges.an@hotmail.com>
2022-05-12 07:12:07 -0300

    up footer and back-to-top

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for(const link of links) {
 }
 
 /* Mudar o header da pagina quando der scroll*/
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window. addEventListener('scroll', function() {
     if (window.scrollY >= navHeight) {
         //scroll maior que altura do header
         header.classList.add('scroll')
@@ -29,7 +29,7 @@ window. addEventListener('scroll', function() {
         //menor que a altura do header
         header.classList.remove('scroll')
     }
-})
+}
 
 /*Testimonials carousel slider swiper*/
 const swiper = new Swiper('.swiper', {
@@ -54,5 +54,27 @@ scrollReveal.reveal(
     #about .image, #about .text, 
     #services header, #services .card, 
     #testimonials header, #testimonials .testimonials
-    #contact .text, #contact .links
-    `, {interval: 100})
+    #contact .text, #contact .links, 
+    footer .brand, footer .social
+    `, 
+    {interval: 100}
+)
+
+/* Botão voltar ao topo */
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if(window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+   
+
+/*function execute */
+window. addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+})
